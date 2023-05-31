0118ca3bc38afbbca7082463edb36f6f708aad63
Alex Alfonso Armengol <AlexAlfonsoArmengol@gmail.com>
2023-05-31 14:18:07 +0200

    Added initial architecture

---
+++ MyFirstCalculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCalculator
+{
+    public class Calculator
+    {
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Substract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+    }
+    // Lo correcto seria "mokkar". Primero test unitario que le ayudan a hacer el codigo,
+    // cuando tiene el codigo implementado hace el rest de integracion.
+}
